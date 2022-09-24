fc0abef700c8f15a06b95bdf79a4cc34d16f5352
Alejandro Armadillo <alex.cel.lemon@gmail.com>
2022-09-24 12:51:40 -0500

    Section 2 Lesson 7: Make a Route and Link to it ===== Learned ===== - css with 2 elements ===== Added ===== - posts.blade.php ===== Modified ===== - Renamed post.blade.php - app.css - web.php

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Nuevo Blog del Curso de Laravel desde Cero</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+    <article>
+        <h1><a href="/post">Mi primer Post Sabrosito</a></h1>
+
+        <p>
+            <strong>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur dignissimos dolor doloribus
+                eaque, fuga laudantium nam nesciunt nisi nostrum nulla obcaecati placeat quae quas repudiandae sapiente
+                sed temporibus voluptas, voluptates?
+            </strong>
+            <strong>Lorem ipsum dolor sit amet,</strong> consectetur adipisicing elit. Aspernatur dignissimos dolor doloribus eaque, fuga laudantium nam nesciunt nisi nostrum nulla obcaecati placeat quae quas repudiandae sapiente sed temporibus voluptas, voluptates?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur dignissimos dolor doloribus eaque, fuga laudantium nam nesciunt nisi nostrum nulla obcaecati placeat quae quas repudiandae sapiente sed temporibus voluptas, voluptates?
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/post">Mi Segundo Post Sabrosito</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur dignissimos dolor doloribus eaque, fuga laudantium nam nesciunt nisi nostrum nulla obcaecati placeat quae quas repudiandae sapiente sed temporibus voluptas, voluptates?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur dignissimos dolor doloribus eaque, fuga laudantium nam nesciunt nisi nostrum nulla obcaecati placeat quae quas repudiandae sapiente sed temporibus voluptas, voluptates?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur dignissimos dolor doloribus eaque, fuga laudantium nam nesciunt nisi nostrum nulla obcaecati placeat quae quas repudiandae sapiente sed temporibus voluptas, voluptates?
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/post">Mi Tercer Post Sabrosito</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur dignissimos dolor doloribus eaque, fuga laudantium nam nesciunt nisi nostrum nulla obcaecati placeat quae quas repudiandae sapiente sed temporibus voluptas, voluptates?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur dignissimos dolor doloribus eaque, fuga laudantium nam nesciunt nisi nostrum nulla obcaecati placeat quae quas repudiandae sapiente sed temporibus voluptas, voluptates?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur dignissimos dolor doloribus eaque, fuga laudantium nam nesciunt nisi nostrum nulla obcaecati placeat quae quas repudiandae sapiente sed temporibus voluptas, voluptates?
+        </p>
+    </article>
+
+</body>
+</html>
