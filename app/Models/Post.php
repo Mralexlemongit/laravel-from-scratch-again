d3da59c8ca417ae652efb977435288e163d97716
Alejandro Armadillo <alex.cel.lemon@gmail.com>
2022-09-24 18:30:49 -0500

    Section 2 Lesson 11 Part 1: Use the Filesystem Class to Read a Directory ===== Learned ===== - Create Models - ModelNotFoundException() - resource_path ===== Added ===== - Model Post ===== Modified ===== - web.php

---
+++ app/Models/Post.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+class Post
+{
+    public static function find($slug)
+    {
+        if (! file_exists($path = resource_path("/posts/{$slug}.html")) ) {
+            throw new ModelNotFoundException();
+        }
+
+        return cache()->remember(
+            "posts.{$slug}",
+            now()->addminutes(20),
+            fn() => file_get_contents($path)
+        );
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -7,13 +8,7 @@ Route::get('/', function () {
 });
 
 Route::get('posts/{post}', function ($slug) {
-    if (! file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html";) ) {
-        return redirect('/');
-    }
-
-    $post = cache()->remember("posts.{$slug}", now()->addminutes(20), fn() => file_get_contents($path));
-
     return view('post', [
-       'post' => $post
-   ]);
+        "post" => Post::find($slug)
+    ]);
 })->where('post', '[A-z_\-]+');
