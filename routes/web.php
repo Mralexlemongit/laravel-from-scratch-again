a995eceef6f982c7c860ff9eb4b9c80632556b6d
Alejandro Armadillo <alex.cel.lemon@gmail.com>
2022-09-24 16:27:28 -0500

    Section 2 Lesson 10: Use Caching for Expensive Operations ===== Learned ===== - "cache()->remember()" - "now()->addminutes()" ===== Modified ===== - web.php

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,11 @@ Route::get('/', function () {
 });
 
 Route::get('posts/{post}', function ($slug) {
-    $path = __DIR__ . "/../resources/posts/{$slug}.html";
-
-    if (! file_exists($path) ) {
+    if (! file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html";) ) {
         return redirect('/');
-//        abort(404);
     }
 
-    $post = file_get_contents($path);
+    $post = cache()->remember("posts.{$slug}", now()->addminutes(20), fn() => file_get_contents($path));
 
     return view('post', [
        'post' => $post
