0c0a67f838b4bf6388718a15d200771944eb1692
Alejandro Armadillo <alex.cel.lemon@gmail.com>
2022-09-24 20:14:53 -0500

    Section 2 Lesson 12 Part 1: Find a Composer Package for Post Metadata ===== Learned ===== - collect() - collect()->map() - static - firstWhere ===== Modified ===== - Models/Post.php - views/post.blade.php

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -25,44 +25,19 @@ class Post
 
     public static function all()
     {
-        $files = File::files(resource_path("posts/"));
-        $posts = [];
-
-        foreach ($files as $file) {
-            $document = YamlFrontMatter::parseFile($file);
-
-            $posts[] = new Post(
+        return collect(File::files(resource_path("posts/")))
+            ->map(fn($file) => YamlFrontMatter::parseFile($file))
+            ->map(fn($document) => new Post(
                 $document->title,
                 $document->excerpt,
                 $document->date,
                 $document->body(),
                 $document->slug,
-            );
-        }
-
-        return $posts;
+            ));
     }
 
     public static function find($slug)
     {
-        if (! file_exists($path = resource_path("/posts/{$slug}.html")) ) {
-            throw new ModelNotFoundException();
-        }
-
-        return cache()->remember(
-            "posts.{$slug}",
-            now()->addminutes(0),
-            function () use ($path) {
-                $document = YamlFrontMatter::parseFile($path);
-
-                return new Post(
-                    $document->title,
-                    $document->excerpt,
-                    $document->date,
-                    $document->body(),
-                    $document->slug,
-                );
-            }
-        );
+        return static::all()->firstWhere('slug', $slug);
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -10,8 +10,13 @@
 </head>
 <body>
 <article>
-    <h1><?= $post->title; ?></h1>
-    <?= $post->body; ?>
+    <h1>
+        <?= $post->title; ?>
+    </h1>
+
+    <div>
+        <?= $post->body; ?>
+    </div>
 </article>
 
 <a href="/"><p>Comeback</p></a>
